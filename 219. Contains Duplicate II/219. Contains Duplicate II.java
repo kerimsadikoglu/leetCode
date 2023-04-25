d1e4d98e556b7a79b7d01691062fd958060dd02a
kerimsadikoglu <kerimsadikoglu33@gmail.com>
2023-04-25 14:46:25 +0300

    Runtime 24 ms Beats 22.17% Memory 54.8 MB Beats 13.1%

---
+++ 219. Contains Duplicate II/219. Contains Duplicate II.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> d = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (i - d.getOrDefault(nums[i], -1000000) <= k) {
+                return true;
+            }
+            d.put(nums[i], i);
+        }
+        return false;
+    }
+}
