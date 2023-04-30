54205a831aadaef0816ae3b22217be393158f945
kerimsadikoglu <kerimsadikoglu33@gmail.com>
2023-04-30 13:14:06 +0300

    Runtime 1 ms Beats 100% Memory 40.3 MB Beats 8.4%

---
+++ 263. Ugly Number/263. Ugly Number.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public boolean isUgly(int num) {
+        if (num == 0) {
+            return false;
+        }
+        while (num != 1) {
+            if (num % 2 == 0) {
+                num = num / 2;
+                continue;
+            }
+            if (num % 3 == 0) {
+                num = num / 3;
+                continue;
+            }
+            if (num % 5 == 0) {
+                num = num / 5;
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
+}
