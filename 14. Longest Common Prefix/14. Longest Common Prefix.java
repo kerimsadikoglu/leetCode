88c0b6d1bae7490d0bf45bb0b738b2669e41bbc9
kerimsadikoglu <kerimsadikoglu33@gmail.com>
2023-05-14 12:49:23 +0300

    Runtime 1 ms Beats 77.53% Memory 40.8 MB Beats 52.60%

---
+++ 14. Longest Common Prefix/14. Longest Common Prefix.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+           StringBuilder longestCommonPrefix = new StringBuilder();
+        // Base condition
+        if (strs == null || strs.length == 0) {
+            return longestCommonPrefix.toString();
+        }
+        // Find the minimum length string from the array
+        int minimumLength = strs[0].length();
+        for (int i = 1; i < strs.length; i++) {
+            minimumLength = Math.min(minimumLength, strs[i].length());
+        }
+        // Loop for the minimum length
+        for (int i = 0; i < minimumLength; i++) {
+            // Get the current character from first string
+            char current = strs[0].charAt(i);
+            // Check if this character is found in all other strings or not
+            for (String str : strs) {
+                if (str.charAt(i) != current) {
+                    return longestCommonPrefix.toString();
+                }
+            }
+            longestCommonPrefix.append(current);
+        }
+        return longestCommonPrefix.toString();
+    }
+}
