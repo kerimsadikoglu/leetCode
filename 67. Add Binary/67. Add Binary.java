b956a2616595e1f7dd11d684d9bdd11d071890bd
kerimsadikoglu <kerimsadikoglu33@gmail.com>
2023-05-16 09:35:07 +0300

    Runtime 2 ms Beats 56.82% Memory 42.7 MB Beats 24%

---
+++ 67. Add Binary/67. Add Binary.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+
+            carry = sum / 2;
+            result.insert(0, sum % 2);
+        }
+
+        return result.toString();
+    }
+}
