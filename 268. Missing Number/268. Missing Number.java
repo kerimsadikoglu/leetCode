ad9620dfa5c5cf6ccae0b150a17a5e35eac5812f
kerimsadikoglu <kerimsadikoglu33@gmail.com>
2023-05-24 09:46:22 +0300

    Runtime 6 ms Beats 28.90% Memory 44.1 MB Beats 5.53%

---
+++ 268. Missing Number/268. Missing Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
