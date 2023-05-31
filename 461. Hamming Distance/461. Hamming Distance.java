8cac8eb50ab6f936e4af0e54d9b98aa34644b141
kerimsadikoglu <kerimsadikoglu33@gmail.com>
2023-05-31 09:14:58 +0300

    Runtime 0 ms Beats 100% Memory 39.8 MB Beats 33.59%

---
+++ 461. Hamming Distance/461. Hamming Distance.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int count = 0;
+        
+        int z = x^y;
+        
+        while(z!=0){
+            if((z&1) == 1){
+                count++;
+            }
+            z = z>>1;
+        }
+        
+        return count;
+    }
+}
